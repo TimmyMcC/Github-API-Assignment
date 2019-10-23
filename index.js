f01a36d8bd92e1ea46ebbb7f9f9911ca5417e02e
TimmyMcC <53845486+TimmyMcC@users.noreply.github.com>
2019-10-23 18:38:25 -0500

    Updated index.js

--- index.js
+++ index.js
@@ -1,39 +1,44 @@
 'use strict';
 
-function getUserInfo(username) {
+function getUserInfo() {
 
-  const searchURL = `https://api.github.com/users/${username}/repos`;
-  fetch(searchURL)
-  .then(response => response.json())
-  .then(responseJson => {
-    displayResults(responseJson);
-  })
+  let url1 = "https://api.github.com/users/";
+  let url2 = "/repos";
+  let username = $('#js-username-id').val();
+  const searchURL = url1 + username + url2;
+
+    fetch(searchURL)
+    .then(( (response) => {
+        if(response.ok) {
+            return response.json();
+        } else {
+            throw new Error(response.statusText);
+        }
+    }))
+    .then(responseJson => displayResults(responseJson))
+
   .catch(err => {
     $('#js-error-message').text(`Something went wrong: ${err.message}`);
   });
 
 }
 
-function displayResults() {
+function displayResults(responseJson) {
 
   $('#js-search-results, #js-error-message').empty();
   console.log(responseJson);
-  $('#js-username').append(`${responseJson[0].owner.login}`).show();
-    responseJson.forEach(repo => {
-      $('#js-repos-list').append(
-        `<li class="result">
-            <a href="${repo.html_url}">${repo.name}</a>
-        </li>`
-      );
-    });
-
+  for (let i = 0; i < responseJson.length; i++) {
+    $('#js-search-results').append(
+      `<li><a href="${responseJson[i].html_url}">${responseJson[i].name}</a></li>`
+    )
+  } 
   $('#js-search-results').removeClass('hidden');
 }
 
 function watchForm() {
   $('#js-search-form').submit(event => {
     event.preventDefault();
-    const username = $('#js-username').val();
+    const username = $('#js-username-id').val();
     getUserInfo(username);
   });
 }
